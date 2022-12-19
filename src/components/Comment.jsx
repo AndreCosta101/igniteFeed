43be80808069f72fed7a9311104202ffdd141b35
AndreCosta <andre.costa101@gmail.com>
2022-12-19 17:36:00 -0300

    feat(style): comment styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,28 +1,32 @@
-import { Trash } from 'phosphor-react';
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export function Comment(){
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/andre.costa101.png" />
+            <img src="https://avatars.githubusercontent.com/u/48858873?v=4" />
 
-            <div className={styles.CommentBox}>
+            <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
-                            <strong>Andre</strong>
+                            <strong>Andre </strong>
                             <time title='11 de maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
                         </div>
+
                         <button title='Deletar comentário'>
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
 
                     <p>Muito bom Devon, parabéns!! </p>
                 </div>
+
                 <footer>
-                    <ThumbsUp/>
-                    Aplaudir <span>20</span>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
                 </footer>
             </div>
         </div>
